6a04d5dda0a20a544db3be31afe54032381ad57f
Andrzej Gębala <andrzej.gebala35@gmail.com>
2022-10-12 08:56:24 +0200

    fixing playername local storage bug

--- dist/script/mian.js
+++ dist/script/mian.js
@@ -4,16 +4,23 @@ const paragraph = document.querySelector(".name-enter__instruction")
 
 
 
-let playerName = localStorage.getItem("playerName", "name") || " "
+let playerName = localStorage.getItem("playerName") || null
 const nameCheck = (e) =>{
    if(e.key === "Enter" && nameInput.value !== ''){
     playerName = nameInput.value;
     paragraph.textContent = `Witaj ${playerName.toLocaleUpperCase()}, wybierz grę dla siebie:`;
-    nameInput.value = "";
     nameInput.classList.add("hide")
     inputLabel.classList.add("hide")
    }
     localStorage.setItem("playerName", playerName);
 }
 
+if(playerName !== null){
+    paragraph.textContent = `Witaj ${playerName.toLocaleUpperCase()}, wybierz grę dla siebie:`;
+    nameInput.classList.add("hide")
+    inputLabel.classList.add("hide")
+}
+
 nameInput.addEventListener("keypress", nameCheck)
+
+
